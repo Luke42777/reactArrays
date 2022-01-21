b85033916c4d3e635d80951f1a7ab0b2f5f7047c
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-21 16:04:34 +0000

    filtering used to list users by sex

--- app.js
+++ app.js
@@ -3,49 +3,78 @@ const data = {
         {
             id: 1,
             age: 23,
-            name: "Johan"
+            name: "Johan",
+            sex: "male",
         },
         {
             id: 2,
             age: 43,
-            name: "Theo"
+            name: "Theo",
+            sex: "male",
         },
         {
             id: 3,
             age: 73,
-            name: "Mirinda"
+            name: "Mirinda",
+            sex: "female",
+        },
+        {
+            id: 4,
+            age: 33,
+            name: "Carolina",
+            sex: "female",
         }
     ]
 }
 
-
-const Item = (props) => {
+const Item = ({user}) => {
     return (
-        <div>
-            <h1>{`User: ${props.user.name}`}</h1>
-            <h2>{`Age: ${props.user.age}`}</h2>
+        <div className="userInfo">
+            <h1>{user.name}</h1>
+            <p><strong>User inforamation:</strong></p>
+            <p>{`Age: ${user.age}`}</p>
+            <p>{`Sex: ${user.sex}`}</p>
+            <br/>
         </div>
     )
 }
 
 class ListItems extends React.Component {
+    state = {
+        select: "all",
+    }
+
+    userList = () => {
+        let users = this.props.data.users;
+       
+            switch(this.state.select) {
+                case "all":
+                   return users.map(user => <Item key={user.id} user={user} />);
+                case "female":
+                   users = users.filter(user =>  user.sex === "female");
+                   return users.map(user => <Item key={user.id} user={user} />);
+                case "male":
+                    users = users.filter(user => user.sex === "male");
+                    return users.map(user => <Item key={user.id} user={user} />);
+              }
+    }
+
+    handleUsersFilter = (opt) => {
+        this.setState({select: opt})
+    }
 
     render() {
         
-        const users = this.props.data.users;
-        const Items = users.map(user => <Item key={user.id} user={user} />);
         return (
-            <>
-                {Items}
+            <> 
+            <button onClick={this.handleUsersFilter.bind(this,"male")}>Show male</button>
+            <button onClick={this.handleUsersFilter.bind(this,"female")}>Show female</button>
+            <button onClick={this.handleUsersFilter.bind(this,"all")}>Show all</button>
+            {this.userList()}
             </>
         )
     }
 }
-
-
-
-
-
 
 
 ReactDOM.render(<React.StrictMode><ListItems data={data} /></React.StrictMode>, document.getElementById("root"));
